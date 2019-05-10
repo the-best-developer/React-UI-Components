c33dc59c02516176753f7f0681f0140acff84c43
the-best-developer <im.dylan.gosnell@gmail.com>
2019-05-10 19:19:25 -0400

    Calculator MVP

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,53 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay.js';
+import NumberButton from './components/ButtonComponents/NumberButton.js';
+import ActionButton from './components/ButtonComponents/ActionButton.js';
+import FunctionButton from './components/ButtonComponents/FunctionButton.js';
+
 
 const App = () => {
   return (
-    <div>
+    <div className="mainCalcContainer">
       <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+
+      {/* Display */}
+      <CalculatorDisplay text="0" buttonStyle="calcDisplay" />
+
+      {/* Numbers container */}
+      <div className="numContainer">
+        {/* Clear */}
+        <ActionButton text="Clear" buttonStyle="actionButton"/>
+
+        {/* 7 8 9 */}
+        <NumberButton text="7" buttonStyle="numButton"/>
+        <NumberButton text="8" buttonStyle="numButton"/>
+        <NumberButton text="9" buttonStyle="numButton"/>
+
+        {/* 4 5 6 */}
+        <NumberButton text="4" buttonStyle="numButton"/>
+        <NumberButton text="5" buttonStyle="numButton"/>
+        <NumberButton text="6" buttonStyle="numButton"/>
+
+        {/* 1 2 3 */}
+        <NumberButton text="1" buttonStyle="numButton"/>
+        <NumberButton text="2" buttonStyle="numButton"/>
+        <NumberButton text="3" buttonStyle="numButton"/>
+
+        {/* 0 */}
+        <ActionButton text="0" buttonStyle="actionButton"/>
+      </div>
+
+      {/* Function buttons */}
+      <div className="funContainer">
+        {/*     ÷ x - + =     */}
+        <FunctionButton text="÷" buttonStyle="funButton"/>
+        <FunctionButton text="x" buttonStyle="funButton"/>
+        <FunctionButton text="-" buttonStyle="funButton"/>
+        <FunctionButton text="+" buttonStyle="funButton"/>
+        <FunctionButton text="=" buttonStyle="funButton"/>
+      </div>
+
     </div>
   );
 };
